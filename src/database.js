cde71545623b107bce04192e178cd2c238cd54b6
Burim Saliji <buz.saliji@gmail.com>
2023-11-02 19:53:35 +1100

    feat: user schema and .env implemented

--- src/database.js
+++ src/database.js
@@ -7,7 +7,8 @@ const mongoose = require('mongoose');
 async function databaseConnect(){
 	try {
         // DB connection can take som time, eg. if DB is in the cloud
-		await mongoose.connect('mongodb://localhost:27017/CatDB');
+		console.log("Connecting to database...");
+		await mongoose.connect(process.env.DB_URI_LOCAL);
 		console.log("Database connected");
 	} catch (error) {
 		console.warn(`databaseConnect failed to connect to DB:\n${JSON.stringify(error)}`);
---
+++ src/models/UserModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: false
+    }
+});
+
+
+// const ModelName = mongoose.model('Name that appears in the mongosh or Cloud Atlas GUI, SchemaThatModelIsBasedOn)
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {
+    User
+}
